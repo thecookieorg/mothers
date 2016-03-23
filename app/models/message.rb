4aa0c7638fef4696204d7d222a29ac4ea727dae3
Marko Manojlovic <marko@thecookieapps.com>
2016-03-23 18:33:17 +0000

    modified new message view

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,6 @@
 class Message < ActiveRecord::Base
-    validates :user_id, presence: true 
+    validates :user_id, presence: true
+    validates :title, presence: true
     belongs_to :user
     has_many :comments, dependent: :destroy
 end
