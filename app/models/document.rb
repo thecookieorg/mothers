3f2326bcd3bf05071e135010d958f8da88049fe4
Marko Manojlovic <marko@thecookieapps.com>
2016-03-30 03:04:15 +0000

    Added messages link on each document

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,5 +2,5 @@ class Document < ActiveRecord::Base
     mount_uploader :attachment, AttachmentUploader
     belongs_to :user
     has_many :messages
-    validates_presence_of :title
+    validates_presence_of :title, :requested_by
 end
